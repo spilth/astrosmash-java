504971d34fed2db5bca45373a765f1f0fd224d20
Brian Kelly <polymonic@gmail.com>
2013-04-07 17:38:40 -0400

    Actor doesn't need a constructor

--- src/main/java/org/spilth/astrosmash/actors/Actor.java
+++ src/main/java/org/spilth/astrosmash/actors/Actor.java
@@ -3,17 +3,12 @@ package org.spilth.astrosmash.actors;
 import org.spilth.astrosmash.World;
 
 public class Actor {
-
 	protected World world;
 
-	public Actor() {
-		super();
+	public void update() {
 	}
-
-	public void update() {}
 
 	public void setWorld(World world) {
 		this.world = world;
 	}
-
 }
